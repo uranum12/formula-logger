05fa7f42d9e35d2f140a01d3073b88c150ab7afd
uranum12 <113694643+uranum12@users.noreply.github.com>
2025-07-19 06:49:53 +0900

    add test server

---
+++ server/go.mod
@@ -0,0 +1,28 @@
+module formula-logger/server
+
+go 1.24.5
+
+require (
+	github.com/eclipse/paho.mqtt.golang v1.5.0
+	github.com/labstack/echo/v4 v4.13.4
+	github.com/mattn/go-sqlite3 v1.14.28
+	github.com/samber/slog-echo v1.16.1
+)
+
+require (
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/samber/lo v1.49.1 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	go.opentelemetry.io/otel v1.29.0 // indirect
+	go.opentelemetry.io/otel/trace v1.29.0 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
+	golang.org/x/net v0.40.0 // indirect
+	golang.org/x/sync v0.14.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
+	golang.org/x/time v0.11.0 // indirect
+)
---
+++ server/main.go
@@ -0,0 +1,212 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	_ "github.com/mattn/go-sqlite3"
+	slogecho "github.com/samber/slog-echo"
+)
+
+func MessageHandler(logger *slog.Logger, db *sql.DB) func(c mqtt.Client, m mqtt.Message) {
+	return func(c mqtt.Client, m mqtt.Message) {
+		topic := m.Topic()
+		payload := string(m.Payload())
+		logger.Info("Recieve message", slog.String("topic", topic), slog.String("payload", payload))
+
+		const sql_insert = `
+			INSERT INTO mqtt_data (time, topic, payload)
+			VALUES (?, ?, ?)
+		`
+
+		now := time.Now().Format(time.RFC3339Nano)
+
+		_, err := db.Exec(sql_insert, now, topic, payload)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func mqtt_server(logger *slog.Logger, db *sql.DB) {
+	const broker = "localhost"
+	const port = 1883
+	const client_id = "mqtt-server"
+
+	const topic = "+"
+	const qos = 1
+
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("tcp://%s:%d", broker, port))
+	opts.SetClientID(client_id)
+	client := mqtt.NewClient(opts)
+
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	} else {
+		logger.Info("Connected to MQTT")
+	}
+	defer client.Disconnect(250)
+
+	token := client.Subscribe(topic, qos, MessageHandler(logger, db))
+	if token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	} else {
+		logger.Info("Subscribed", "topic", topic)
+	}
+
+	for {
+		time.Sleep(1 * time.Second)
+	}
+}
+
+type Item struct {
+	Time    string `json:"time"`
+	Payload string `json:"payload"`
+}
+
+type Res struct {
+	Data []Item `json:"data"`
+}
+
+func api_server(logger *slog.Logger, db *sql.DB) {
+	e := echo.New()
+
+	e.Use(slogecho.New(logger))
+	e.Use(middleware.Recover())
+
+	e.GET("/:topic", func(c echo.Context) error {
+		topic := c.Param("topic")
+		limit_param := c.QueryParam("limit")
+
+		const sql_select = `
+			SELECT time, payload FROM mqtt_data
+			WHERE topic = ?
+		`
+
+		const sql_select_limit = `
+			SELECT time, payload FROM (
+				SELECT time, payload FROM mqtt_data
+				WHERE topic = ?
+				ORDER BY time DESC
+				LIMIT ?
+			)
+			ORDER BY time ASC
+		`
+
+		var data []Item
+
+		if limit_param == "" {
+			rows, err := db.Query(sql_select, topic)
+			if err != nil {
+				panic(err)
+			}
+			defer rows.Close()
+
+			for rows.Next() {
+				var item Item
+
+				err = rows.Scan(&item.Time, &item.Payload)
+				if err != nil {
+					panic(err)
+				}
+
+				data = append(data, item)
+			}
+
+			return c.JSON(http.StatusOK, Res{Data: data})
+		}
+
+		limit, err := strconv.Atoi(limit_param)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]string{
+				"error": "invalid limit parameter",
+			})
+		}
+
+		rows, err := db.Query(sql_select_limit, topic, limit)
+		if err != nil {
+			panic(err)
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			var item Item
+
+			err = rows.Scan(&item.Time, &item.Payload)
+			if err != nil {
+				panic(err)
+			}
+
+			data = append(data, item)
+		}
+
+		return c.JSON(http.StatusOK, Res{Data: data})
+	})
+
+	e.Logger.Fatal(e.Start(":1234"))
+}
+
+func main() {
+	loc, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		panic(err)
+	}
+	now := time.Now().In(loc)
+
+	if err := os.Mkdir("log", 0755); err != nil && !os.IsExist(err) {
+		panic(err)
+	}
+	log_path := filepath.Join("log", now.Format(time.RFC3339)+".log")
+
+	f, err := os.Create(log_path)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	multiWriter := io.MultiWriter(os.Stdout, f)
+
+	logger := slog.New(slog.NewJSONHandler(multiWriter, nil))
+
+	if err := os.Mkdir("data", 0755); err != nil && !os.IsExist(err) {
+		panic(err)
+	}
+	db_path := filepath.Join("data", now.Format(time.RFC3339)+".db")
+
+	db, err := sql.Open("sqlite3", db_path)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	const sql_create = `
+		CREATE TABLE IF NOT EXISTS mqtt_data (
+			id INTEGER PRIMARY KEY,
+			time TEXT,
+			topic TEXT,
+			payload TEXT
+		)
+	`
+	_, err = db.Exec(sql_create)
+	if err != nil {
+		panic(err)
+	}
+
+	go mqtt_server(logger, db)
+	go api_server(logger, db)
+
+	for {
+		time.Sleep(1 * time.Second)
+	}
+}
