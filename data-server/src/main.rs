565ef1a70d221170dc2f422b6a841349b98a35de
uranum12 <113694643+uranum12@users.noreply.github.com>
2025-10-23 08:09:13 +0900

    add CLI args

--- data-server/src/main.rs
+++ data-server/src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    match env::args().nth(1).as_deref() {
+        Some("gps") => println!("GPS"),
+        Some("spi") => println!("SPI"),
+        Some("save") => println!("Save"),
+        Some("mqtt") => println!("MQTT"),
+        Some("api") => println!("API"),
+        _ => println!("other"),
+    }
 }
