a3785065a8b72f54e3a8be755629f20b2688a4e8
uranum12 <113694643+uranum12@users.noreply.github.com>
2025-10-23 00:24:22 +0900

    refactor Json class

--- client/include/json.hpp
+++ client/include/json.hpp
@@ -4,6 +4,7 @@
 #include <stdint.h>
 
 #include <memory>
+#include <string_view>
 
 #include <pico/time.h>
 
@@ -11,24 +12,21 @@
 
 class Json {
 public:
-    Json(const char* topic)
+    explicit Json(std::string_view topic)
         : root_(cJSON_CreateObject(), cJSON_Delete),
           payload_(cJSON_CreateObject()) {
-        cJSON_AddStringToObject(root_.get(), "topic", topic);
+        cJSON_AddStringToObject(root_.get(), "topic", topic.data());
         cJSON_AddItemToObject(root_.get(), "payload", payload_);
     }
 
-    void add(const char* key, double value) {
-        cJSON_AddNumberToObject(payload_, key, value);
-    }
-    void add(const char* key, const char* value) {
-        cJSON_AddStringToObject(payload_, key, value);
+    void add(std::string_view key, double value) {
+        cJSON_AddNumberToObject(payload_, key.data(), value);
     }
 
     void addTime(absolute_time_t time) {
         uint64_t usec_total = to_us_since_boot(time);
-        uint32_t sec = usec_total / 1'000'000;
-        uint32_t usec = usec_total % 1'000'000;
+        uint32_t sec = static_cast<uint32_t>(usec_total / 1'000'000);
+        uint32_t usec = static_cast<uint32_t>(usec_total % 1'000'000);
         add("sec", sec);
         add("usec", usec);
     }
@@ -38,13 +36,6 @@ public:
             return false;
         }
         return cJSON_PrintPreallocated(root_.get(), buf, size, false);
-    }
-
-    cJSON* get() {
-        return root_.get();
-    }
-    const cJSON* get() const {
-        return root_.get();
     }
 
 private:
